ea17c9fdf671428c866edfbcc394d83af7b9ddcd
genie117 <geniewong45@gmail.com>
2025-09-02 10:21:48 +1000

    Update run_v2.sh

--- run_v2.sh
+++ run_v2.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Activate virtual environment
-source ~/myenv/bin/activate
+# Activate the correct virtual environment
+source ~/envs/ppb_env/bin/activate
 
-# Confirm Python version and transformers version
+# Confirm Python environment
 echo "Using Python:"
 which python
 python --version
 python -c "import transformers; print('Transformers version:', transformers.__version__)"
+python -c "import torch; print('Torch version:', torch.__version__, '| CUDA available:', torch.cuda.is_available())"
 
 # Run the training script
 python clinicalbert_train_with_symptom_dict.py
